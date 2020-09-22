662d31149bacb13ecb9644b6c98cbfeba66b6e08
PedroDLF <pedrodlafuente@gmail.com>
2020-09-22 18:22:24 +0200

    init
    init repo

---
+++ stamp-doc-sign-verify-api/src/main/java/com/confidencepath/stampdocsignverifyapi/StampDocSignVerifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.confidencepath.stampdocsignverifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StampDocSignVerifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StampDocSignVerifyApiApplication.class, args);
+	}
+
+}
---
+++ stamp-doc-sign-verify-api/src/test/java/com/confidencepath/stampdocsignverifyapi/StampDocSignVerifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.confidencepath.stampdocsignverifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StampDocSignVerifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
